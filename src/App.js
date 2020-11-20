af20af4137546c58caede9caaa33ecf3434d2f2a
Bridgette <turner.bridgette@yahoo.com>
2020-11-19 21:23:39 -0700

    routing?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import AddContact from "./components/AddContact";
+import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
+import EditContact from "./components/EditContact";
+import { Route, Router, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="App App-header">
+      <Router>
+        <Route exact path="/" component={ContactList} />
+        <Route exact path="/contact/:id" component={Contact} />
+        <Route path="/contact/:id/edit" component={EditContact} />
+        <Route path="/contact/add" component={AddContact} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+export default function ContactList() {
+  const [contacts, setContacts] = useState([]);
+  const url = `http://localhost:8000/api/contacts`;
+
+  const getContacts = async (e) => {
+    try {
+      const res = await fetch(url);
+      const contacts = await res.json();
+      setContacts(contacts);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  console.log(contacts);
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  return (
+    <div>
+      <button>Add contact</button>
+      <ul className="Contacts">
+        {contacts.map((c) => (
+          <li key={c.id}>
+            <strong>{c.name}</strong>
+            <p>{c.phone}</p>
+            <p>{c.email}</p>
+            <p>
+              {c.street} {c.zip}
+            </p>
+            <p>{c.note}</p>
+            <button>delete contact</button>
+            <button>edit contact</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
